14c124751fef99b9155d05b5bd1b229377e151bd
15515151 <vip1961000366@qq.com>
2026-07-27 03:42:46 +0800

    release(android): prepare version 1.5.0

--- packages/android-client/app/build.gradle.kts
+++ packages/android-client/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.yueby.musictogether"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.1"
+        versionCode = 8
+        versionName = "1.5.0"
     }
 
     buildFeatures {
