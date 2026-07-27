d141502f5f389a1a85d01cb5d99d0af75d4fd160
LiuYunLingNai <w2623944562@163.com>
2026-07-27 21:04:20 +0800

    release(android): prepare version 1.8.0

--- packages/android-client/app/build.gradle.kts
+++ packages/android-client/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.yueby.musictogether"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.7.0"
+        versionCode = 11
+        versionName = "1.8.0"
     }
 
     flavorDimensions += "distribution"
